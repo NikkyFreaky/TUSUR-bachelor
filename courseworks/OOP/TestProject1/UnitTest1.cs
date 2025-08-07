2b96b58244052d7ead6653ff04b0ecda19fcaae4
Nikita Zlobin <nsz@2i.tusur.ru>
2025-08-07 23:15:32 +0700

    feat: add OOP coursework

---
+++ courseworks/OOP/TestProject1/UnitTest1.cs
@@ -0,0 +1,180 @@
+using Microsoft.Win32;
+using Moq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ColorPickerApp.Classes;
+
+namespace ColorPickerApp.Tests
+{
+    // Класс тестов для ImageHandler
+    [TestFixture]
+    public class ImageHandlerTests
+    {
+        // Объекты, которые будут использоваться в тестах
+        private ImageHandler imageHandler;
+        private OpenFileDialog openFileDialog;
+
+        // Этот метод будет вызываться перед каждым тестом, он инициализирует тестовые объекты
+        [SetUp]
+        public void SetUp()
+        {
+            imageHandler = new ImageHandler();
+            openFileDialog = new OpenFileDialog();
+        }
+
+        /*
+        // Тестовый случай для проверки работы открытия изображения с подходящим расширением файла
+        [Test]
+        public void OpenImage_WithValidImage_ReturnsImage()
+        {
+            // Подготовка тестовых данных
+            string imagePath = "C:\\Users\\Nikita\\Desktop\\0.jpg";
+            string extension = Path.GetExtension(imagePath).ToLower();
+
+            // Создаем макет OpenFileDialog
+            var openFileDialogMock = new Mock<OpenFileDialog>();
+
+            // Устанавливаем ожидаемое поведение макета
+            openFileDialogMock.Setup(o => o.ShowDialog()).Returns(true);
+            openFileDialogMock.Setup(o => o.FileName).Returns(imagePath);
+
+            // Вызов тестируемого метода
+            BitmapImage result = null;
+            if (IsImageValid(extension))
+            {
+                result = imageHandler.OpenImage(openFileDialogMock.Object);
+            }
+
+            // Проверка результата
+            Assert.IsNotNull(result);
+        }
+        */
+
+        // Тестовый случай для проверки работы открытия изображения с неподходящим расширением файла
+        [Test]
+        public void OpenImage_WithInvalidImage_ReturnsNull()
+        {
+            // Подготовка тестовых данных
+            string imagePath = "F:\\Documents\\Visual Studio 2022\\Projects\\WpfApp1\\TestProject1\\Images\\11.heic";
+            BitmapImage image = new BitmapImage(new Uri(imagePath));
+            string extension = Path.GetExtension(imagePath).ToLower();
+
+            // Вызов тестируемого метода
+            BitmapImage result = null;
+            if (IsImageValid(extension))
+            {
+                result = imageHandler.OpenImage(openFileDialog);
+            }
+
+            // Проверка результата
+            Assert.IsNull(result);
+        }
+
+        // Вспомогательный метод для проверки расширения файла изображения
+        private bool IsImageValid(string extension)
+        {
+            // Фильтр файлов для диалогового окна выбора изображений
+            const string ImageFilter = "Файлы изображений (*.png;*.jpeg;*.jpg;*.gif;*.raw;*.tiff;*.bmp;*.psd)|*.png;*.jpeg;*.jpg;*.gif;*.raw;*.tiff;*.bmp;*.psd";
+
+            // Разделение фильтра на расширения
+            string[] extensions = ImageFilter.Split('|')[1].Split(';');
+
+            // Проверка соответствия расширения изображения фильтру
+            foreach (string validExtension in extensions)
+            {
+                if (extension == validExtension.ToLower())
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    // Класс тестов для ColorExtractor
+    [TestFixture]
+    public class ColorExtractorTests
+    {
+        // Объекты, которые будут использоваться в тестах
+        private ColorExtractor _colorExtractor;
+        private Mock<BitmapSource> _mockBitmapSource;
+
+        // Этот метод будет вызываться перед каждым тестом, он инициализирует тестовые объекты
+        [SetUp]
+        public void SetUp()
+        {
+            _colorExtractor = new ColorExtractor();
+
+            // Создаем макет BitmapSource
+            _mockBitmapSource = new Mock<BitmapSource>();
+            _mockBitmapSource.SetupGet(x => x.PixelWidth).Returns(100);
+            _mockBitmapSource.SetupGet(x => x.PixelHeight).Returns(100);
+            _mockBitmapSource.Setup(x => x.Format).Returns(PixelFormats.Bgr32);
+
+            // Создаем фейковые пиксели красного цвета
+            byte[] fakePixels = new byte[] { 0, 0, 255, 255 };
+            _mockBitmapSource.Setup(x => x.CopyPixels(It.IsAny<Int32Rect>(), It.IsAny<Array>(), It.IsAny<int>(), It.IsAny<int>()))
+                .Callback(new Action<Int32Rect, Array, int, int>((rect, pixels, stride, offset) =>
+                {
+                    fakePixels.CopyTo((byte[])pixels, 0);
+                }));
+        }
+
+        // Тестовый случай для проверки работы извлечения цвета из изображения
+        [Test]
+        public void ExtractColor_WhenCalled_ShouldReturnRedColor()
+        {
+            // Подготовка тестовых данных
+            int x = 50;
+            int y = 50;
+
+            // Вызов тестируемого метода
+            var result = _colorExtractor.ExtractColor(_mockBitmapSource.Object, x, y);
+
+            // Проверка результата
+            Assert.That(result, Is.EqualTo(Colors.Red));
+        }
+
+        // Тестовый случай для проверки работы получения цвета в указанной точке
+        [Test]
+        public void GetColorAtPoint_WhenCalled_ShouldReturnRedColor()
+        {
+            // Подготовка тестовых данных
+            var point = new Point(50, 50);
+
+            // Вызов тестируемого метода
+            var result = _colorExtractor.GetColorAtPoint(_mockBitmapSource.Object, point, 100, 100);
+
+            // Проверка результата
+            Assert.That(result, Is.EqualTo(Colors.Red));
+        }
+    }
+
+    // Класс тестов для TopColorFinder
+    [TestFixture]
+    public class TopColorFinderTests
+    {
+        // Тестовый случай для проверки работы поиска топовых цветов
+        [Test]
+        public void GetTopColors_ReturnsTopColors()
+        {
+            // Подготовка тестовых данных
+            var bitmapSource = new BitmapImage(new Uri("F:\\Documents\\Visual Studio 2022\\Projects\\WpfApp1\\TestProject1\\Images\\1.jpg"));
+            var colorExtractor = new ColorExtractor();
+
+            var topColorFinder = new TopColorFinder();
+
+            // Вызов тестируемого метода
+            bitmapSource.Freeze(); // Замораживаем объект BitmapSource
+            var result = topColorFinder.GetTopColors(bitmapSource, 3, colorExtractor);
+
+            // Проверка результата
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Count, Is.GreaterThan(0));
+            Assert.That(result.Count, Is.EqualTo(3));
+        }
+    }
+
+}
---
+++ courseworks/OOP/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using ColorPickerApp.Classes;
+using ColorPickerApp.Interfaces;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ColorPickerApp
+{
+    public partial class MainWindow : Window
+    {
+        // Объявление объектов для обработки изображения, клонирования, извлечения цвета и поиска топ цветов
+        private readonly IImageHandler _imageHandler;
+        private readonly IImageCloner _imageCloner;
+        private readonly IColorExtractor _colorExtractor;
+        private readonly ITopColorFinder _topColorFinder;
+
+        public MainWindow()
+        {
+            // Инициализация компонентов
+            InitializeComponent();
+
+            // Инициализация объектов для обработки изображения, клонирования, извлечения цвета и поиска топ цветов
+            _imageHandler = new ImageHandler();
+            _imageCloner = new ImageCloner();
+            _colorExtractor = new ColorExtractor();
+            _topColorFinder = new TopColorFinder();
+        }
+
+        private void ImgUpload_Click(object sender, RoutedEventArgs e)
+        {
+            // Открытие изображения через ImageHandler
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            ImageHandler imageHandler = new ImageHandler();
+            BitmapImage image = imageHandler.OpenImage(openFileDialog);
+
+            // Если изображение не было загружено, то выходим из обработчика событий
+            if (image == null) return;
+
+            // Отображение загруженного изображения
+            ImgDynamic.Source = image;
+
+            // Запуск задачи в отдельном потоке
+            Task.Run(() =>
+            {
+                // Клонирование изображения
+                BitmapSource clone = _imageCloner.CloneBitmapSource(image);
+
+                // Включение индикатора обработки
+                Dispatcher.Invoke(() => ProcessingBar.Visibility = Visibility.Visible);
+
+                // Извлечение топ 5 цветов из изображения
+                List<Color> topColors = _topColorFinder.GetTopColors(clone, 5, _colorExtractor);
+
+                // Обновление интерфейса
+                Dispatcher.Invoke(() =>
+                {
+                    // Отображение топ цветов
+                    DisplayTopColors(topColors);
+                    // Выключение индикатора обработки
+                    ProcessingBar.Visibility = Visibility.Hidden;
+                });
+            });
+        }
+
+        private void CopyColor_Click(object sender, RoutedEventArgs e)
+        {
+            // Если нажатая кнопка, то копирование соответствующего значения цвета в буфер обмена
+            if (sender is Button button)
+            {
+                Clipboard.SetText(button.Name == "CopyRGB" ? TextRGB.Text : TextHEX.Text);
+            }
+        }
+
+        private void ImgDynamic_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Получение точки нажатия и извлечение цвета в этой точке
+            Point clickPoint = e.GetPosition(ImgDynamic);
+            Color color = _colorExtractor.GetColorAtPoint((BitmapSource)ImgDynamic.Source, clickPoint, ImgDynamic.ActualWidth, ImgDynamic.ActualHeight);
+
+            // Отображение информации о выбранном цвете
+            DisplayColorInfo(color);
+        }
+
+        private void DisplayColorInfo(Color color)
+        {
+            // Заполнение превью цвета и отображение значения в формате RGB и HEX
+            ColorPreview.Background = new SolidColorBrush(color);
+            TextRGB.Text = $"RGB: {color.R}, {color.G}, {color.B}";
+            TextHEX.Text = $"HEX: #{color.R:X2}{color.G:X2}{color.B:X2}";
+        }
+
+        private void DisplayTopColors(List<Color> colors)
+        {
+            // Массивы контейнеров и текстовых блоков для отображения цветов
+            Border[] colorContainers = { TopColor1, TopColor2, TopColor3, TopColor4, TopColor5 };
+            TextBlock[] colorTexts = { TopColorText1, TopColorText2, TopColorText3, TopColorText4, TopColorText5 };
+
+            // Обновление интерфейса в основном потоке
+            Dispatcher.Invoke(() =>
+            {
+                // Цикл по всем полученным цветам
+                for (int i = 0; i < colors.Count; i++)
+                {
+                    // Установка цвета фона для каждого контейнера и отображение RGB кода цвета
+                    colorContainers[i].Background = new SolidColorBrush(colors[i]);
+                    colorTexts[i].Text = $"RGB: {colors[i].R}, {colors[i].G}, {colors[i].B}";
+                }
+            });
+        }
+    }
+}
